7f5308ad6e8d089edbe38b37bf5a6a2df761239c
Felipe Poirier <felipe.poirier.v@mail.pucv.cl>
2025-11-15 02:53:00 -0300

    Modificación y corrección de archivo ofertas.h

--- INCLUDE/ofertas.h
+++ INCLUDE/ofertas.h
@@ -1,15 +1,24 @@
-#ifndef OFERTA_H
-#define OFERTA_H
+#ifndef OFERTAS_H
+#define OFERTAS_H
+
+#include "usuario.h"
+#include "subastas.h"
 
 typedef struct {
     int idOferta;
     int idUsuario;
     float monto;
     char fecha[20];
-    int estado; // 0 válida, 1 ganadora, 2 descartada
+    int estado;   // 0 = válida, 1 = ganadora, 2 = descartada
 } Oferta;
 
-Oferta* crearOferta(int idUsuario, float monto);
-void destruirOferta(Oferta* o);
+// Crear una oferta para un producto (idProducto) usando el usuario actual
+void crearOferta(int idProducto, Usuario *usuarioActivo);
+
+// Listar todas las ofertas asociadas a un id de producto
+void listarOfertas(int idProducto);
+
+// Elegir oferta ganadora entre las ofertas existentes
+void elegirOfertaGanadora(int idProducto);
 
 #endif
